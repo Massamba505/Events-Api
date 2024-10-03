2808b2df5ea4f0501873b9239c1e6b7c97432402
SOMEONE1703 <siphelel.1703@gmail.com>
2024-10-03 23:38:53 +0200

    myevents

--- routes/event.route.js
+++ routes/event.route.js
@@ -11,15 +11,17 @@ router.get('/upcoming-events', allUpcomingEvents);
 router.get('/inprogress-events', allInProgressEvents);
 router.get('/past-events', allPastEvents);
 router.get('/sort-by', sort);
-router.get('/:id',EventDetails);
-router.get('/myevents', MyEvents);
+
 
 // Protected routes
+router.get('/myevents',authenticate, MyEvents);
 router.post('/new', authenticate, upload.array('images'), createEvent);
 router.post('/new-no-image', authenticate, createEventNoImage);
 router.put('/update/:id', authenticate, updateEvent);
 router.delete('/:id', deleteEvent);
 router.patch('/:id/cancel', cancelEvent);
 // router.get('/myevents',authenticate, MyEvents);
+
+router.get('/:id',EventDetails);
 
 module.exports = router;
