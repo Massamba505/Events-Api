90ca980b31a0366e49e9cd8a1b6085a17e1c3a81
Ritanzwe <140893680+Ritanzwe@users.noreply.github.com>
2024-10-29 16:00:12 +0200

    Update ticket.route.js

--- routes/ticket.route.js
+++ routes/ticket.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { buyTicket2, confirmPayment, cancelTicket, requestRefund, getTicket, getAllTickets, cancelPayment } = require('../controllers/ticket.controller');
+const { buyTicket2, confirmPayment, scanTicket, requestRefund, getTicket, getAllTickets, cancelPayment } = require('../controllers/ticket.controller');
 const { authenticate } = require('../middlewares/protect.middleware');
 
 router.post('/buy', authenticate, buyTicket2);
@@ -12,6 +12,8 @@ router.get('/cancel/:session_id', cancelPayment);
 router.post('/:ticketId/refund', authenticate, requestRefund);
 
 router.get('/:ticketId', authenticate, getTicket);
+
+router.post('/:ticketId/accept', authenticate, scanTicket);
 
 router.get('/', authenticate, getAllTickets);
 
